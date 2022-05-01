715204988dfad193ba4c854ef64da42b26e4bbdc
janghyoseog <bestdevhyo1225@gmail.com>
2022-05-01 22:07:32 +0900

    refactor: object 싱글턴 클래스로 리팩토링

--- infrastructure/jpa/src/main/kotlin/com/hs/product/mapper/ProductEventMapper.kt
+++ infrastructure/jpa/src/main/kotlin/com/hs/product/mapper/ProductEventMapper.kt
@@ -3,32 +3,31 @@ package com.hs.product.mapper
 import com.hs.product.entity.ProductEvent
 import com.hs.product.entity.ProductEventJpaEntity
 
-class ProductEventMapper {
-    companion object {
-        fun toJpaEntity(source: ProductEvent): ProductEventJpaEntity {
-            return ProductEventJpaEntity(
-                productId = source.productId,
-                eventStatus = source.eventStatus,
-                isPublished = source.isPublished
-            )
-        }
+object ProductEventMapper {
 
-        fun toDomain(source: ProductEventJpaEntity): ProductEvent {
-            return ProductEvent.create(
-                id = source.id!!,
-                productId = source.productId,
-                eventStatus = source.eventStatus,
-                isPublished = source.isPublished,
-                publishedAt = source.publishedAt,
-                createdAt = source.createdAt,
-            )
-        }
+    fun toJpaEntity(source: ProductEvent): ProductEventJpaEntity {
+        return ProductEventJpaEntity(
+            productId = source.productId,
+            eventStatus = source.eventStatus,
+            isPublished = source.isPublished
+        )
+    }
 
-        fun reflectDomainProperty(source: ProductEventJpaEntity, target: ProductEvent) {
-            target.reflectPropertyAfterStorageIsSaved(
-                id = source.id!!,
-                createdAt = source.createdAt
-            )
-        }
+    fun toDomain(source: ProductEventJpaEntity): ProductEvent {
+        return ProductEvent.create(
+            id = source.id!!,
+            productId = source.productId,
+            eventStatus = source.eventStatus,
+            isPublished = source.isPublished,
+            publishedAt = source.publishedAt,
+            createdAt = source.createdAt,
+        )
+    }
+
+    fun reflectDomainProperty(source: ProductEventJpaEntity, target: ProductEvent) {
+        target.reflectPropertyAfterStorageIsSaved(
+            id = source.id!!,
+            createdAt = source.createdAt
+        )
     }
 }
