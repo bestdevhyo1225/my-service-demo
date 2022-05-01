53c0b7c0e6ad8c95ec743a3b9ddf196509787b9a
janghyoseog <bestdevhyo1225@gmail.com>
2022-05-01 17:35:25 +0900

    refactor: External Event Worker (Consumer, Publisher) 구조 분리

--- external-app/event-publisher-worker/build.gradle.kts
+++ external-app/event-publisher-worker/build.gradle.kts
@@ -9,8 +9,7 @@ dependencies {
     // Project
     implementation(project(":common"))
     implementation(project(":domain"))
-    implementation(project(":infrastructure:mongodb"))
-    implementation(project(":infrastructure:redis"))
+    implementation(project(":infrastructure:jpa"))
 
     // Spring Boot Stater
     implementation("org.springframework.boot:spring-boot-starter-amqp")
